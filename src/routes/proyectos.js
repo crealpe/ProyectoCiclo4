de3d5ddea3dbc4c3d792d884c89efc3d1d324610
Carmen Realpe <carmenrealpe@yahoo.es>
2021-12-18 22:26:27 -0500

    despliegue

--- src/routes/proyectos.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router=express.Router()
-
-router.get("/proyectos", (req, res)=> { //ruta editable
-    res.send("Proyectos en la base de datos")
-})
-
-module.exports=router;
